3793be6adfa7d835a4788a8576ea4ec177e278c9
Vegard Økland <vegard.okland@schibsted.com>
2018-12-04 08:55:29 +0100

    Challenge 1

---
+++ 2018/1.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const process = require('process');
+
+const input = String(fs.readFileSync('inputs/1.txt')).trim();
+
+const numbers = input.split('\n')
+      .map(s => parseInt(s))
+      .filter(n => typeof n === 'number')
+
+const sum = numbers.reduce((accl, n) => accl += n, 0)
+
+console.log(`Solution 1.1: \t ${sum}`)
+
+// part 2
+function* getNumbers () {
+  let _numbers = [0]
+
+  let n = 0;
+  let last = 0;
+  while(true) {
+    _numbers[n + 1] = _numbers[n] + numbers[n % numbers.length];
+    yield _numbers[n + 1]
+    n+=1;
+  }
+}
+
+const seen = {}
+for (let n of getNumbers()) {
+  if (seen[n]) {
+    console.log(`Solution 1.1: \t ${n}`)
+    break
+  } else {
+    seen[n] = true
+  }
+}
+
+console.log()
--- 2018/index.js
+++ 2018/index.js
@@ -1,5 +1,6 @@
 console.log('Advent of code 2018');
 console.log()
 
+require('./1');
 require('./2');
 require('./3');
