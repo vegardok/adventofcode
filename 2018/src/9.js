b856666e014e242f409746690229db0aef11f9a9
Vegard Økland <vegard.okland@schibsted.com>
2018-12-09 16:16:20 +0100

    Faster challenge 9

--- 2018/src/9.js
+++ 2018/src/9.js
@@ -1,12 +1,12 @@
 const { Range } = require('immutable')
 
-
 function getHighScore(PLAYERS, STOP) {
   let currentMarble = 0
   let currentPlayer = 0
 
   let score = Range(0, PLAYERS).reduce((m, i) => { m[i] = 0; return m }, {})
-  let game = [0]
+  let game = new Uint32Array(STOP)
+  let boardSize = 1
 
   for (let i = currentMarble + 1; i < STOP + 1; i++) {
     if (i % 10000 === 0) {
@@ -15,25 +15,31 @@ function getHighScore(PLAYERS, STOP) {
 
 
     if (i % 23 === 0) {
-      currentMarble = (currentMarble - 7) % game.length
+      currentMarble = (currentMarble - 7) % boardSize
       if (currentMarble < 0) {
-        currentMarble = game.length + currentMarble
+        currentMarble = boardSize + currentMarble
       }
-
-
       score[currentPlayer] = score[currentPlayer] + i + game[currentMarble]
-      game.splice(currentMarble, 1)
-    } else {
-      let next = (currentMarble + 2) % (game.length)
-      if (next === 0) {
-        next = game.length
-      }
-      game.splice(next, 0, i)
 
+      // todo
+      game.copyWithin(currentMarble, currentMarble + 1, boardSize)
+      boardSize -= 1
+    } else {
+      let next = (currentMarble + 2) % (boardSize)
+      // console.log({ i, boardSize, next, })
+      if (next === 0) {
+        next = boardSize
+      }
+
+      game.copyWithin(next + 1, next, boardSize)
+
+      game[next] = i
       currentMarble = next
       currentPlayer = (currentPlayer + 1) % PLAYERS
-
+      boardSize += 1
     }
+    // console.log(boardSize, game.slice(0, boardSize ).join(', '))
+    // console.log()
   }
 
   const winner = Object.values(score).sort((a, b) => b - a)
@@ -50,11 +56,9 @@ function getHighScore(PLAYERS, STOP) {
 // 30 players; last marble is worth 5807 points: high score is 37305
 
 
-getHighScore(9, 25)
-
 const a1 = getHighScore(459, 71320)
 console.log(`Solution 9.1: \t ${a1}`)
 
 
-// const a2 = getHighScore(459, 71320 * 100)
-// console.log(`Solution 9.1: \t ${a2}`)
+const a2 = getHighScore(459, 71320 * 100)
+console.log(`Solution 9.1: \t ${a2}`)
