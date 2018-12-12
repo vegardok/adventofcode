cac4a8a1be8b7ab8825fda18f01f929cd58b1b9a
Vegard Økland <vegard.okland@schibsted.com>
2018-12-12 17:15:05 +0100

    wip

--- 2018/src/12.js
+++ 2018/src/12.js
@@ -1,80 +1,155 @@
+const { Range, Map } = require('immutable')
+
 const fs = require('fs')
-const { Map, Range } = require('immutable')
 
 let input =String(fs.readFileSync(__dirname + '/../inputs/12.txt'))
     .trim()
     .split('\n')
 
-const pots = input[0].split(': ')[1] // .split('') //.map(c => c === '#')
-
-function trim(str, c, leading=true) {
-  let i;
-  if (leading) {
-    i = 0;
-    while(str.charAt(i) === c){
-      i++
-    }
-    return [str.substr(i), i]
-  } else {
-    i = str.length-1
-    while (str.charAt(i) === c) {
-      i -= 1
-    }
-    return [str.substring(0, i+1), Math.abs(i - (str.length-1))]
-  }
-}
-
-// function trimaa(, c, leading=true) {
-//   let i;
-//   if (leading) {
-//     i = 0;
-//     while(strt(i) === c){
-//       i++
-//     }
-//     return [str.substr(i), i]
-//   } else {
-//     i = str.length-1
-//     while (str.charAt(i) === c) {
-//       i -= 1
-//     }
-//     return [str.substring(0, i+1), Math.abs(i - (str.length-1))]
-//   }
-// }
-
-
-const GENS = 50000000000
+let data = input[0].split(': ')[1].split('').map(c => c === '#' ? 1 : 0)
 
 const rules = input.slice(2).reduce((rules, rule) => {
   let [match, result] = rule.split(' => ')
+  match = match.split('').map(c => c === '#' ? '1' : '0').join('')
   result = result === '#'
-  return rules.set(match, result)
-}, Map()).toJS()
+  rules[match] = result
+  return rules
+}, {})
 
-const getState = (somePots, i) => {
-  const k = Range(i-2, i+3).map(i => somePots[i] || '.').join('')
-  // console.log(k, rules[k])
-  return rules[k] ? '#' : '.';
+
+let rules2 = Map()
+
+Range(0, 2).forEach(
+  i1 => Range(0, 2).forEach(
+    i2 => Range(0, 2).forEach(
+      i3 => Range(0, 2).forEach(
+        i4 => Range(0, 2).forEach(
+          i5 => {
+            rules2 = rules2.setIn([i1, i2, i3, i4, i5], rules[[i1, i2, i3, i4, i5].join('') ] ? 1 : 0)
+          }
+        )
+      )
+    )
+  )
+)
+
+rules2 = rules2.toJS()
+
+
+const getState = (all, i) => {
+  return rules2[all[i-2] || 0][all[i-1] || 0][all[i] || 0][all[i+1] || 0][all[i+2] || 0]
+}
+
+const GENS = 100000 // 20
+const PADDING = 20
+const EDGE = 3
+
+let offset = -PADDING
+
+const pots = new Uint8Array((PADDING * 2) + data.length)
+pots.set(data, PADDING)
+
+
+let arrays = {
+  prev: pots.slice(),
+  current: pots.slice()
+}
+
+function shiftRight(a) {
+  const tmp = new Uint8Array(a.length + PADDING)
+  tmp.set(a, PADDING)
+  return tmp
+
+}
+function trimLeft(a) {
+  const tmp = a.slice(a.indexOf(1) - PADDING, a.length)
+  return tmp
+}
+
+function shiftLeft(a) {
+  const tmp = new Uint8Array(a.length + PADDING)
+  tmp.set(a)
+  return tmp
 
 }
 
-let removedLeading;
-let offset = 0
+function trimRight(a) {
+  const tmp = new Uint8Array(a.length - PADDING)
+  tmp.set(a.slice(0, a.length - PADDING))
+  return tmp
+}
 
-const finalPots = Range(1, GENS + 1).reduce((workingPots, i) => {
-  if (i % 1000 === 0) {
-    console.log(i)
+for (let i = 0; i < GENS + 1; i++) {
+  arrays.current = new Uint8Array(arrays.prev.length)
+  for(let x = 0; x < arrays.prev.length; x++) {
+    arrays.current[x] = getState(arrays.prev, x)
   }
-  let newPots = Range(-2, workingPots.length + 3).map(i => getState(workingPots, i)).join('');
-  [newPots, removedLeading] = trim(newPots, '.');
-  [newPots] = trim(newPots, '.', false)
-  offset -= (2 - removedLeading)
-  // console.log(i, 2 - removedLeading, offset, newPots)
-  return newPots
-}, pots)
+
+  if (i % 10000 === 0) {
+    console.log(i, new Date(), { offset, size: arrays.current.length }, Array.from(arrays.current).reduce((sum, c, i) => {
+      return sum + (c === 1 ? (i + (offset)) : 0)
+    }, 0))
+    // console.log(Array.from(arrays.current).join(''))
+  }
+
+  if (arrays.current.indexOf(1) <= EDGE) {
+    // console.log(1, arrays.current.indexOf(1) , EDGE)
+    // console.log(Array.from(arrays.current).join(''))
+    arrays.current = shiftRight(arrays.current)
+    // arrays.prev = shiftRight(arrays.prev)
+    offset = offset - PADDING
+    // console.log (1, { offset })
+    // console.log(Array.from(arrays.current).join(''))
+
+    // console.log()
+  }
+
+  if (arrays.current.indexOf(1) > (PADDING * 2)) {
+    // console.log({ foo: 2}, arrays.current.indexOf(1), (PADDING * 2))
+    // console.log(Array.from(arrays.current).join(''))
+
+    arrays.current = trimLeft(arrays.current)
+    // arrays.prev = trimLeft(arrays.prev)
+    offset = offset + PADDING
+    // console.log (2, { offset })
+
+    // console.log(Array.from(arrays.current).join(''))
+    // console.log()
+  }
+
+  if ((arrays.current.length - arrays.current.lastIndexOf(1)) < EDGE) {
+    // console.log(3, (arrays.current.length - arrays.current.lastIndexOf(1)), EDGE)
+    // console.log(Array.from(arrays.current).join(''))
+    arrays.current = shiftLeft(arrays.current)
+    // console.log(3, { offset })
+    // arrays.prev = shiftLeft(arrays.prev)
+    // console.log(Array.from(arrays.current).join(''))
+    // console.log()
+  }
+
+  if ((arrays.current.length - arrays.current.lastIndexOf(1)) > (2 * PADDING)) {
+    // console.log(4, (arrays.current.length - arrays.current.lastIndexOf(1)), (2 * PADDING))
+    // console.log(Array.from(arrays.current).join(''))
+    arrays.current = trimRight(arrays.current)
+    // console.log(4, { offset })
+    // arrays.prev = trimRight(arrays.prev)
+    // console.log(Array.from(arrays.current).join(''))
+    // console.log()
+  }
 
 
-console.log(finalPots, offset)
-console.log(finalPots.split('').reduce((sum, c, i) => {
-  return sum + (c === '#' ? (i + offset) : 0)
+  // const tmpa = arrays.prev
+  arrays.prev = arrays.current
+  // arrays.prev = tmpa
+}
 
+
+console.log('done', { offset }) // 2767
+
+// not 6662
+
+var result = Array.from(arrays.current)
+console.log(result.join(''))
+console.log((result).reduce((sum, c, i) => {
+  return sum + (c === 1 ? (i + (offset)) : 0)
 }, 0))
