4f9e1bdebab1046f749904dc8c2da7b26e724cd4
Vegard Økland <vegard.okland@schibsted.com>
2018-12-16 17:26:18 +0100

    2017: 10 part 2

--- 2017/src/10.py
+++ 2017/src/10.py
@@ -1,38 +1,31 @@
 from collections import deque
-
-key = [int(i) for i in open('../inputs/10.txt').readline().split(',')]
-index = [i for i in range(256)]
+from functools import reduce
 
 
+def knotHash(key):
+    skip = 0
+    rotateCount = 0
+    index = deque([i for i in range(256)])
+    for _ in range(64):
+        for count in key:
+            s = [index[i] for i in range(count)]
+            s.reverse()
 
-# key = [3, 4, 1, 5]
-# index = [i for i in range(5)]
+            for i, v in enumerate(s):
+                index[i] = v
 
+            rotateN = (count + skip) % len(index)
+            rotateCount += rotateN
+            index.rotate(-1 * rotateN)
+            skip = skip + 1
 
-current = 0
-skip = 0
+    index.rotate(rotateCount)
+    index = list(index)
 
-print("Starting")
+    return ''.join([format(reduce((lambda accl, x: accl ^ x), index[i:i+16]), '02x') for i in range(0, len(index), 16)])
 
-for count in key:
-    s = [index[(current + i) % len(index)] for i in range(count)]
+padding = [17,31,73,47,23]
 
-    print("count", count)
-    print("current", current)
-    print("s", s)
-    s.reverse()
-    print("s", s)
+key2 = [ord(i) for i in open('../inputs/10.txt').readline().strip()] + padding
 
-    for i, v in enumerate(s):
-        index[(i+current) % (len(index))] = v
-
-    print('index', index)
-
-    current = (current + count + skip) % len(index)
-    skip = skip + 1
-    print("skip", skip)
-
-
-    print()
-
-print(index[0:2], index[0] * index[1])
+print(knotHash(key2))
