e43c37206e05732e945ae4b937835fe362b392d7
Vegard Økland <vegard.okland@schibsted.com>
2019-12-10 16:56:08 +0545

    Day 5 part 2

--- src/aoc_2019/5.clj
+++ src/aoc_2019/5.clj
@@ -3,6 +3,9 @@
             [aoc-2019.computer :as c]
             [aoc-2019.inputs :as input]))
 
-(println "Day 5")
-(println "Part 1")
-(c/computer-loop input/day5-input)
+(let []
+  (println "Day 5")
+  (println "Part 1")
+  (c/computer-loop input/day5-input) ;; 1 => 15508323
+  (c/computer-loop input/day5-input) ;; 5 => 9006327
+  nil)
--- src/aoc_2019/computer.clj
+++ src/aoc_2019/computer.clj
@@ -1,67 +1,92 @@
 (ns aoc-2019.computer
   (:require [clojure.test :as test]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [aoc-2019.inputs :as inputs]))
 
-(defn- pos-mode [n program] (nth program (nth program n)))
-(defn- imm-mode [n program] (nth program n))
+(defn- input [pointer program mode]
+  (if (= mode :pos-mode)
+    (nth program (nth program pointer))
+    (nth program pointer)))
+
+(defn- math-fn [func]
+  (fn [a-mode b-mode c-mode]
+    (fn [pointer program]
+      (let [a (input (+ pointer 1) program a-mode)
+            b (input (+ pointer 2) program b-mode)
+            out (input (+ pointer 3) program :imm-mode)]
+        {:program (assoc program out (func a b))
+         :pointer (+ pointer 4) } ))))
+
+(defn- read-input [a-mode b-mode c-mode]
+  (defn- foo [pointer program]
+    (let [out (input (+ pointer 1) program :imm-mode)]
+      {:program (assoc program out (Integer/parseInt (read-line)))
+       :pointer (+ pointer 2) } )))
+
+(defn- print-out [a-mode b-mode c-mode]
+  (fn [pointer program]
+    (let [a (input (+ pointer 1) program a-mode)]
+      (println a)
+      { :program program :pointer (+ pointer 2) })))
+
+(defn- exit [a-mode b-mode c-mode]
+  (fn [pointer program]
+    {:program program :pointer nil }))
+
+(defn- jump [jump-fn]
+  (fn [a-mode b-mode c-mode]
+    (fn [pointer program]
+      (let [a (input (+ pointer 1) program a-mode)
+            b (input (+ pointer 2) program b-mode)]
+        {:program program
+         :pointer (if (jump-fn a) b (+ pointer 3)) }))))
+
+(defn- compare-op [comp-fn]
+  (fn [a-mode b-mode c-mode]
+    (fn [pointer program]
+      (let [a (input (+ pointer 1) program a-mode)
+            b (input (+ pointer 2) program b-mode)
+            out (input (+ pointer 3) program :imm-mode)]
+        {:program (assoc program out (if (comp-fn a b) 1 0))
+         :pointer (+ pointer 4) }))))
 
 (defn parse-op [op]
-  (let [str-op (format "%05d" op)
+  (let [ops
+        {
+         "01" (math-fn +)
+         "02" (math-fn *)
+         "03" read-input
+         "04" print-out
+         "05" (jump #(not= 0 %))
+         "06" (jump #(= 0 %))
+         "07" (compare-op #(< % %2))
+         "08" (compare-op #(= % %2))
+         "99" exit
+         }
+        str-op (format "%05d" op)
         op (clojure.string/join (drop 3 str-op))
-        modes {\0 pos-mode
-               \1 imm-mode }
+        modes { \0 :pos-mode \1 :imm-mode }
         mode-a (get modes (nth str-op 2))
         mode-b (get modes (nth str-op 1))
         mode-c (get modes (nth str-op 0))
-        io-op (or (= op "03") (= op "04"))
-        ops {"01" +
-             "02" *
-             "03" (fn [a b] (Integer/parseInt (read-line)))
-             "04" (fn [a b] (println a) a)}]
-    { :fn (get ops op)
-     :op op
-     :mode-a mode-a
-     :mode-b mode-b
-     :mode-c mode-c
-     :seek (if io-op 2 4)
-     :output (if io-op #(+ 1 %) #(+ 3 %))
-     }))
-
-
-(defn computer [pointer program]
-  (let [op (parse-op (nth program pointer))
-        op-fn (get op :fn)
-        a ((get op :mode-a) (+ 1 pointer) program)
-        b (try
-            ((get op :mode-b) (+ 2 pointer) program)
-            (catch Exception e nil))
-        output-adress (get program ((get op :output) pointer))
-        result (op-fn a b)]
-    (assoc program output-adress result)))
+        ]
+    ((get ops op) mode-a mode-b mode-c)))
 
 (defn computer-loop
   ([program] (computer-loop 0 program))
   ([program noun verb] (computer-loop 0 (assoc program 1 noun 2 verb)))
   ([pointer program]
-   (let [parsed-op (parse-op (nth program pointer))
-         op (get parsed-op :op)
-         new-pointer (+ pointer (get parsed-op :seek))]
-     (if (= op "99")
+   (let [op (parse-op (nth program pointer))
+         next-step (op pointer program)
+         next-pointer (get next-step :pointer)
+         next-program (get next-step :program)]
+     (if (not next-pointer)
        program
-       (let [program (computer pointer program)]
-         (recur new-pointer program))))))
-
-(def input-test1 [1,0,0,0,99])
-(def input-test2 [2,3,0,3,99])
-(def input-test3 [2,4,4,5,99,0])
-(def input-test4 [1,1,1,4,99,5,6,0,99])
-(def input-test5 [3,0,4,0,99])
-(def input-test6 [1002,4,3,4,33])
-
+       (recur next-pointer next-program)))))
 
 (test/deftest examples-part-1
-  (test/is (= (computer-loop input-test1) [2,0,0,0,99]))
-  (test/is (= (computer-loop input-test2) [2,3,0,6,99]))
-  (test/is (= (computer-loop input-test3) [2,4,4,5,99,9801]))
-  (test/is (= (computer-loop input-test4) [30,1,1,4,2,5,6,0,99]))
-  (test/is (= (computer-loop input-test6) [1002,4,3,4,99])))
+  (test/is (= (computer-loop [1,0,0,0,99]) [2,0,0,0,99]))
+  (test/is (= (computer-loop [2,3,0,3,99]) [2,3,0,6,99]))
+  (test/is (= (computer-loop [2,4,4,5,99,0]) [2,4,4,5,99,9801]))
+  (test/is (= (computer-loop [1,1,1,4,99,5,6,0,99]) [30,1,1,4,2,5,6,0,99]))
+  (test/is (= (computer-loop [1002,4,3,4,33]) [1002,4,3,4,99])))
