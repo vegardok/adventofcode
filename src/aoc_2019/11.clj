ee88f36adf4bd0bd06bf472459eb76df70a7f22c
Vegard Økland <vegard.okland@schibsted.com>
2019-12-13 12:24:07 +0545

    fixup! Refactor state

--- src/aoc_2019/11.clj
+++ src/aoc_2019/11.clj
@@ -5,9 +5,7 @@
             [aoc-2019.inputs :as inputs]))
 
 
-(defn paint- [panel x y color]
-  (let [row (assoc (get panel x {}) y color)]
-    (assoc panel x row)))
+
 
 (defn move [robot-state]
   (let [moves {0 (fn [[x y]] [(- x 1) y])
@@ -20,16 +18,17 @@
       (assoc robot-state :position new-pos))))
 
 
-(defn camera-fn [robot-state panel-state]
+(defn camera-fn [state]
   (fn []
-    (let [{ [x y] :position } @robot-state
-          panel @panel-state]
+    (let [{panel :panel
+           robot :robot } @state
+          { [x y] :position } robot]
       (-> panel
           (get x {})
           (get y 0)))))
 
-(defn get-panel [panel-state]
-    (let [p @panel-state
+(defn get-panel [state]
+    (let [{ p :panel } @state
           lines-index (-> p keys sort)
           lines (map (fn [line-n]
                        (let [line (get p line-n)
@@ -38,60 +37,65 @@
           ]
       (string/join "\n" (map string/join lines))))
 
-(defn paint [robot-state panel-state color]
-  (let [{position :position} @robot-state
-        [x y] position]
-    (swap! panel-state
-           (fn [old-panel] (paint- old-panel x y color)))))
+(defn paint- [panel x y color]
+  (let [row (assoc (get panel x {}) y color)]
+    (assoc panel x row)))
 
-(defn turn-and-move [robot-state input]
-    (let [dir-map {0 dec 1 inc}]
-      (swap! robot-state
-             (fn [old-robot-state]
-               (-> old-robot-state
-                   (update :direction #(mod ((get dir-map input) %) 4))
-                   (move))))))
+(defn paint [state color]
+  (let [ { { [x y] :position } :robot } state]
 
-(defn write [input-flipper robot-state panel-state ]
+    (update state :panel (fn [old-panel] (paint- old-panel x y color)))))
+
+(defn turn-and-move [state input]
+  (let [dir-map {0 dec 1 inc}]
+
+    (update state :robot
+            (fn [old-robot-state]
+              (-> old-robot-state
+                  (update :direction #(mod ((get dir-map input) %) 4))
+                  (move))))))
+
+(defn write [state]
   (fn [op]
-    (let [flip @input-flipper]
-      (case flip
-        0 (paint robot-state panel-state op)
-        1 (turn-and-move robot-state op))
-      (swap! input-flipper #(mod (+ 1 %) 2)))))
-
-
+    (swap!
+     state
+     (fn [prev-state]
+       (let [{flip :input-flip} prev-state
+             new-state (case flip
+                         0 (paint prev-state op)
+                         1 (turn-and-move prev-state op))]
+         (update new-state :input-flip #(mod (+ 1 %) 2)))))))
 
 (defn print-results []
   (time
    (do
      (println "Day 11")
-     (let [panel-state (atom {})
-           robot-state (atom { :direction 0 :position [0 0]})
-           input-flipper (atom 0)
-           write (write input-flipper robot-state panel-state)
-           read (camera-fn robot-state panel-state)]
+     (let [state (atom {:panel {}
+                        :robot { :direction 0 :position [0 0]}
+                        :input-flip 0
+                        })
+           write (write state)
+           read (camera-fn state)]
        (c/computer-loop inputs/day11-input read write)
-       (print "Part 1 " (count (filter #(or (= \0 %) (= \1 %)) (get-panel panel-state))))
+       (print "Part 1 " (count (filter #(or (= \0 %) (= \1 %)) (get-panel state))))
        (println))
-
-     (let [panel-state (atom {0 {0 1}})
-           robot-state (atom { :direction 0 :position [0 0]})
-           input-flipper (atom 0)
-           write (write input-flipper robot-state panel-state)
-           read (camera-fn robot-state panel-state)]
+     (let [state (atom {:panel {0 { 0 1}}
+                        :robot { :direction 0 :position [0 0]}
+                        :input-flip 0
+                        })
+           write (write state)
+           read (camera-fn state)]
        (c/computer-loop inputs/day11-input read write)
        (println "Part 2 ")
-       (println (-> (get-panel panel-state)
+       (println (-> (get-panel state)
                     (string/replace \0 \ )
                     (string/replace \1 \#)))
        (println))
-
-     ;;   ;;   ##   #### ###  #  # ####   ##  ##  ###
-     ;;   ;;  #  #  #    #  # # #     #    # #  # #  #
-     ;;   ;;  #     ###  #  # ##     #     # #    #  #
-     ;;   ;;  #     #    ###  # #   #      # #    ###
-     ;;   ;;  #  #  #    #    # #  #    #  # #  # # #
-     ;;   ;;  ##    #### #    #  # ####  ##   ##  #  #
+     ;;   ##   #### ###  #  # ####   ##  ##  ###
+     ;;  #  #  #    #  # # #     #    # #  # #  #
+     ;;  #     ###  #  # ##     #     # #    #  #
+     ;;  #     #    ###  # #   #      # #    ###
+     ;;  #  #  #    #    # #  #    #  # #  # # #
+     ;;  ##    #### #    #  # ####  ##   ##  #  #
      )))
 (print-results)
