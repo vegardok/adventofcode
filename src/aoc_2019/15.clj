f3894cc8fefc8b3f4eb393f5d3510a01077b5e66
Vegard Økland <vegard.okland@schibsted.com>
2019-12-21 20:53:22 +0100

    Day 15 wip

--- src/aoc_2019/15.clj
+++ src/aoc_2019/15.clj
@@ -60,21 +60,19 @@
                                (range from-y (inc to-y)))))
                            (range from-x (inc to-x)))))))
 
-(defn paint- [panel x y color]
-  (let [row (assoc (get panel x {}) y color)]
-    (assoc panel x row)))
-
-(def state-map
-  {0 :wall
-   1 :hall
-   2 :oxygen })
-
 (defn write-status [state]
-  (fn [op]
-    (swap!
-     state
-     (fn [prev-state]
-       (assoc prev-state :status (get state-map op))))))
+  (let [state-map
+        {0 :wall
+         1 :hall
+         2 :oxygen }]
+    (fn [op]
+      (swap!
+       state
+       (fn [state]
+         (let [{location :location
+                direction :direction} state
+               { x :x y :y } (move direction location)]
+           (assoc-in state [:panel x y] (get state-map op))))))))
 
 
 (defn move [op location]
@@ -85,21 +83,17 @@
     3 (update location :y inc)) ;; east
   )
 
-(def move-map
-  { 0 3
-   1 2
-   2 0
-   3 1 })
-
-(def foo
-  { 0 :north
-   1 :south
-   2 :west
-   3 :east })
-
-(def bar
-  { :wall 0
-   :hall 1 })
+(defn get-valid-neighbour-moves
+  [{panel :panel
+    location :location
+    { x :x y :y } location
+    }]
+  (->>
+   (range 4)
+   (map (fn [op] [op (move op location)]))
+   (filter (fn [[op { x :x y :y}]] (= :hall (get-in panel [x y] ))))
+   (map first)
+   ))
 
 (defn get-unknown-neighbour-moves
   [{panel :panel
@@ -111,51 +105,14 @@
    (map (fn [op] [op (move op location)]))
    (filter (fn [[op { x :x y :y}]] (nil? (get-in panel [x y] ))))))
 
-(defn get-valid-neighbour-moves
-  [{panel :panel
-    location :location
-    { x :x y :y } location
-    }]
-  (->>
-   (range 4)
-   (map (fn [op] [op (move op location)]))
-   (filter (fn [[op { x :x y :y}]] (= :hall (get-in panel [x y] ))))))
-
-
-(defn update-location-after-walk [state]
-  (let [{location :location
-         direction :direction
-         status :status
-         } state
-        moved (= status :hall)
-        ]
-    (if (nil? status)
-      state
-      (let [ { x :x y :y } (move direction location)]
-        (-> state
-            (assoc-in [:panel x y] status)
-            (assoc :location
-                   (if moved
-                     (move direction location)
-                     location)))))))
-
-
-(defn get-path-move [state]
-  (if false ;; TODO move to front
-    state
-    #dbg
-    (get-in state [:queue 0])))
-
-
-(def turn-around-map
-  {0 1
-   1 0
-   2 3
-   3 2 })
-
 (defn get-explore-move
   [state]
-  (let [{exploring :exploring
+  (let [turn-around-map {0 1
+                         1 0
+                         2 3
+                         3 2}
+
+        {exploring :exploring
          location :location
          status :status
          direction :direction
@@ -166,120 +123,86 @@
       (get turn-around-map direction)
       (-> moves first first))))
 
-(defn move-back-to-start [queue]
-  (->> queue
-      reverse
-      (map #(get turn-around-map %))
-      vec))
+(defn stop-exploring [state]
+  (let [{ status :status
+         exploring :exploring
+         location :location
+         } state
+        move (get-explore-move state)
+        ]
+    (and (nil? move) (= location exploring))))
 
-(move-back-to-start [0 0 2 2])
-
-
-(defn update-direction [state]
-  (let [{exploring :exploring
-         queue :queue
-         location :location} state
-        is-exploring (not (nil? exploring))
+(defn update-explore-direction [state]
+  (let [{ status :status
+         exploring :exploring
+         } state
+        move (get-explore-move state)
         ]
     (-> state
-        (assoc :direction
-               (if is-exploring
-                 (get-explore-move state)
-                 (get-path-move state)))
-        (update :visited
-                #(if is-exploring
-                   %
-                   (let [{ x :x y :y } (move (first queue) location)]
-                     (assoc-in % [x y] true))))
-        (update :exploring
-                #(if is-exploring
-                   %
-                  (move (first queue) location)))
-        (update :path-taken
-                #(if is-exploring
-                   %
-                   (conj % (first queue))))
-        (update :queue
-                #(if is-exploring
-                   %
-                   (vec (rest %))))
-        (update :iter inc)
+        (assoc :direction move)
+        (update :exploring #(if (nil? move) nil %))
         )))
 
+(defn update-location-after-walk [state]
+  (let [{ direction :direction } state]
+    (if (nil? direction)
+      state
+      (let [{location :location
+             panel :panel} state
+            { x :x y :y } (move direction location)
+            status (get-in panel [x y])
+            moved (= status :hall)
+            ]
+        (if moved
+          (assoc state :location {:x x :y y})
+          state)))))
 
-(defn update-explore-state [state]
-  (let [explore-move (get-explore-move state)]
-    (if (nil? explore-move)
-      (assoc state :exploring nil)
-      state)))
-
-(defn update-queue [state]
-  (let [{exploring :exploring
-         queue :queue
-         panel :panel
-         visited :visited
-         } state
-        moves (get-valid-neighbour-moves state)
-        unexplored-moves (map
-                          first
-                          (filter
-                           (fn [[_ {x :x y :y }]]
-                             (not
-                              (get-in visited [x y]))) moves))]
-    (if (nil? exploring)
-      (let [new-queue (apply conj queue unexplored-moves)]
-        #dbg
-        (-> state
-            (assoc :queue new-queue)
-            ;; (update :queue #(apply conj % moves) )
-            ))
-      state)))
-
-(defn update-exploring [state]
-  (let [{exploring :exploring
-         direction :direction
-         location :location
-         queue :queue
-         } state
-        explore-move (move direction location)
-        ]
-    ;; TODO: wait until droid is in correct pos, walking to "front"
-    (if (nil? exploring)
-      (assoc state :exploring explore-move)
-      state)))
-
-
-(defn get-movement [state]
+(defn explore [state]
   (fn []
     (let
         [new-state
          (swap!
           state
           (fn [state]
-            (let [{ status :status } state]
-              #dbg ^{:break/when (not (empty? (:queue state)))}
-              (->> state
-                   update-location-after-walk
-                   update-explore-state
-                   update-queue
-                   update-direction
-                   ))))]
-      (print-game-state new-state)
-      #dbg ^{:break/when (nil? (:direction new-state))}
+            (let [{ exploring :exploring } state
+                  stop (stop-exploring state)]
+              (if (nil? exploring)
+                state
+                (->> state
+                     update-location-after-walk
+                     update-explore-direction
+                     )
+                ))))]
       (inc (:direction new-state)))))
 
-(let [state (atom {:location {:x 0 :y 0}
-                   :status nil
-                   :panel {}
-                   :visited {0 {0 true}}
-                   :direction nil
-                   :queue []
-                   :path-taken []
-                   :exploring {:x 0 :y 0}
+(defn explore-program
+  [program location]
+  (let [state (atom {:location location
+                     :status nil
+                     :panel {}
+                     :direction nil
+                     :exploring location
+                     :iter 0
+                     })
+        write-status (write-status state)
+        get-movement (explore state)
+        [first-program step] (c/computer-step-by-step program get-movement write-status)
+        ]
 
-                   :iter 0
-                   })
-      write-status (write-status state)
-      get-movement (get-movement state)]
-  ;; (c/computer-loop inputs/day15-input get-movement write-status)
-  )
+    (let [[next-program state]
+          (loop [next-program first-program
+                 step step]
+            (let [
+                  [next-program step] (step)
+                  state-o @state
+                  stop (stop-exploring state-o)
+                  ]
+              (if stop
+                [next-program state]
+                (recur next-program step))))]
+          (print-game-state @state)
+          (println (get-valid-neighbour-moves @state))
+          [next-program @state ]
+          )))
+
+(explore-program inputs/day15-input { :x 0 :y 0 })
